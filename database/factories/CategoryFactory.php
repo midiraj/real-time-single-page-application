c090c6324f2f51c647d860c2106ca3214daeebb5
midiraj <shekh.deep@gmail.com>
2020-02-29 21:04:47 +0800

    Factory and Seed

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -2,11 +2,14 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
+use App\User;
 use App\Model\Category;
 use Faker\Generator as Faker;
 
 $factory->define(Category::class, function (Faker $faker) {
+    $word = $faker->word;
     return [
-        //
+        'name' => $word,
+        'slug' => str_slug($word)
     ];
 });
